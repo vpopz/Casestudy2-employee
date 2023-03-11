74547c08102d5d269a8bfd5258cf7db7bd2955df
VAISAKH <vaisakh1996v@gmail.com>
2023-03-11 22:46:57 +0530

    undo changes in put

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ app.delete('/api/employeelist/:id', async (req,res)=>{
 
 //TODO: Update  a employee data from db by using api '/api/employeelist'
 //Request body format:{name:'',location:'',position:'',salary:''}
-app.put('/api/employeelist/:id', async (req, res)=>{
+app.put('/api/employeelist', async (req, res)=>{
     let data = await employeeModel.findOneAndUpdate({"_id":req.body._id}, req.body);
     res.json(data);
 });
